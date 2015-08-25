736a159defd4d47899c084005aec22ceab94ee84
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-08-25 13:29:15 +0300

    angular - added statistics controller methods

--- src/modules/statistic/statistic.js
+++ src/modules/statistic/statistic.js
@@ -53,11 +53,23 @@
         return o;
     }
 
-    function statisticController() {
-        var s = this;
+    function statisticController($scope, $firebaseArray, DBC) {
+        var ref = DBC.getRef().child('profiles');
 
-        s.getProfiles = function () {
-            return true;
-        };
+        var now = new Date; // now
+        now.setHours(0);   // set hours to 0
+        now.setMinutes(0); // set minutes to 0
+        now.setSeconds(0); // set seconds to 0
+        var startOfDay = Math.floor(now);
+        //var startOfDay = now;
+
+        now.setHours(24);   // set hours to 0
+        now.setMinutes(60); // set minutes to 0
+        now.setSeconds(60); // set seconds to 0
+        var endOfDay = Math.floor(now);
+
+
+        $scope.dailyProfiles = $firebaseArray(ref.orderByChild("timestamp").startAt(startOfDay).endAt(endOfDay));
+        $scope.currentDate = new Date();
     }
 })();
